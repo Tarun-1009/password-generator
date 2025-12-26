ad424290f27e9ccd69dd96aaa04973d251b05ba8
Tarun1009 <tarunkumar.pkl1009@gmail.com>
2025-12-26 21:07:35 +0530

    enhance css of page

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,23 @@
 import react from 'react'
 import './App.css'
+import lock from './assets/lock.png'
 
 function App() {
   return (
     <div className="App">
+      <img src={lock} width="100" height="100"/>
       <h1>Password Generator</h1>
-      <p1>Generate secure passwords easily!</p1>
+      <p>Generate secure passwords easily!</p>
       <div className='password-container'>
         <div className='password-display'>
           <input type="text" ></input>
           <button>⟳</button>
         </div>
-        <button>Copy Password</button>
+        <button>Copy</button>
       </div>
+      <div className='length'> 
         <label>Password Length</label>
+      </div>
       <input type="range" min="6" max="100" />
       <label className='checkbox'>
       Uppercase<input type="checkbox" />
