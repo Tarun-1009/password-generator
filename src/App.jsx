3cd27328aaf5c01b65f268ce150fd3aa93334db3
Tarun1009 <tarunkumar.pkl1009@gmail.com>
2025-12-26 17:13:24 +0530

    add input fields

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import react from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
+    <div className="App">
+      <h1>Password Generator</h1>
+      <p1>Generate secure passwords easily!</p1>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <div>
+          <input type="text" ></input>
+          <button>⟳</button>
+        </div>
+        <button>Copy Password</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div>
+        <label>Password Length</label>
+        <input type="number" min="4" max="20" />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <input type="slider" min="6" max="100" />
+      <input type="checkbox" />Uppercase
+      <input type="checkbox" />Lowercase
+      <input type="checkbox" />Numbers
+      <input type="checkbox" />Symbols
+    </div>
   )
 }
-
 export default App
